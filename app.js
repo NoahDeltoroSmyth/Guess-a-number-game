98729ea5843b8f9ba1297f69eb4f0352afd09c49
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-04 17:28:10 -0700

    added guesses left countdown

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const userInput = document.getElementById('user-input');
 const Button = document.getElementById('button');
 const userMessage = document.getElementById('user-message');
-let guessesLeft = document.getElementById('guesses-left');
+const guessesLeft = document.getElementById('guesses-left');
 
 
 const randomNum = (Math.floor(Math.random() * 20)) + 1;
 console.log(randomNum);
-let initialGuess = 0;
+let numOfGuesses = 4;
 
 Button.addEventListener('click', () => {
-  initialGuess++;
+  numOfGuesses--;
+  guessesLeft.textContent=numOfGuesses;
   const userGuess = Number(userInput.value);
-  
   
   
 
@@ -20,18 +20,20 @@ Button.addEventListener('click', () => {
     Button.disabled = true;
   }
 
-  else if (initialGuess === 4) {
-    userMessage.textContent = 'Out of guesses!';
+  else if (numOfGuesses === 0) {
+    userMessage.textContent = 'You are out of guesses! Better luck next time.';
     Button.disabled = true;
   }
 
   else if (userGuess > randomNum) {
     userMessage.textContent = 'Too high, try again!';
+    guessesLeft.textContent = `You have ${numOfGuesses} tries left.`;
     Button.disbaled = false;
   }
 
   else if (userGuess < randomNum) {
     userMessage.textContent = 'Too low, try again!';
+    guessesLeft.textContent = `You have ${numOfGuesses} tries left.`;
     Button.disabled = false;
   }
 
