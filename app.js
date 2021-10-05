afe61280735d819f6d213bed83826f1fecef52d7
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-04 18:02:20 -0700

    fixed eslint errors and commented out cypress

--- app.js
+++ app.js
@@ -5,36 +5,36 @@ const guessesLeft = document.getElementById('guesses-left');
 
 
 const randomNum = (Math.floor(Math.random() * 20)) + 1;
-console.log(randomNum);
+//console.log(randomNum);
 let numOfGuesses = 4;
 
 Button.addEventListener('click', () => {
-  numOfGuesses--;
-  guessesLeft.textContent=numOfGuesses;
-  const userGuess = Number(userInput.value);
+    numOfGuesses--;
+    guessesLeft.textContent=numOfGuesses;
+    const userGuess = Number(userInput.value);
   
   
 
-  if (userGuess === randomNum) {
-    userMessage.textContent = 'Correct, you won!';
-    Button.disabled = true;
-  }
+    if (userGuess === randomNum) {
+        userMessage.textContent = 'Correct, you won!';
+        Button.disabled = true;
+    }
 
-  else if (numOfGuesses === 0) {
-    userMessage.textContent = 'You are out of guesses! Better luck next time.';
-    Button.disabled = true;
-  }
+    else if (numOfGuesses === 0) {
+        userMessage.textContent = 'You are out of guesses! Better luck next time.';
+        Button.disabled = true;
+    }
 
-  else if (userGuess > randomNum) {
-    userMessage.textContent = 'Too high, try again!';
-    guessesLeft.textContent = `You have ${numOfGuesses} tries left.`;
-    Button.disbaled = false;
-  }
+    else if (userGuess > randomNum) {
+        userMessage.textContent = 'Too high, try again!';
+        guessesLeft.textContent = `You have ${numOfGuesses} tries left.`;
+        Button.disbaled = false;
+    }
 
-  else if (userGuess < randomNum) {
-    userMessage.textContent = 'Too low, try again!';
-    guessesLeft.textContent = `You have ${numOfGuesses} tries left.`;
-    Button.disabled = false;
-  }
+    else if (userGuess < randomNum) {
+        userMessage.textContent = 'Too low, try again!';
+        guessesLeft.textContent = `You have ${numOfGuesses} tries left.`;
+        Button.disabled = false;
+    }
 
 });
